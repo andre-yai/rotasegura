56701955b286cd57190fcb08836ad71ec7d57b41
subjective <subjective@riseup.net>
2015-09-13 03:36:18 -0300

    Add zones CSV

--- load.js
+++ load.js
@@ -3,13 +3,17 @@ var db = mongojs('rotasegura', ['ocorrencias']);
 
 var csv = require('fast-csv');
 
-/* Carregar ocorrências do Jornal do Campus */
-csv.fromPath('csv/ocorrencias-jornaldocampus.csv').on('data', function (data) {
-  db.ocorrencias.insert({
-    loc: [parseFloat(data[0]), parseFloat(data[1])],
-    hora: new Date(data[2]),
-    tipo: data[3],
-    info: data[4],
-    auto: true
+var load = function (path) {
+  csv.fromPath(path).on('data', function (data) {
+    db.ocorrencias.insert({
+      loc: [parseFloat(data[0]), parseFloat(data[1])],
+      hora: new Date(data[2]),
+      tipo: data[3],
+      info: data[4],
+      auto: true
+    });
   });
-});
+}
+
+load('csv/ocorrencias.csv');
+load('csv/zonas.csv');
